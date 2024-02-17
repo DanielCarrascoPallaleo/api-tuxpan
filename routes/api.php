3376cd013243b7c0c1c31ced340df319b4b17ba6
DanielCarrascoPallaleo <carrascopallaleo.daniel@gmail.com>
2024-02-16 22:28:53 -0400

    se agregan las rutas de la api

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\TaskController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +16,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::post('register', [AuthController::class, 'register']);
+Route::post('login', [AuthController::class, 'login']);
+
+Route::group(['middleware' => ['auth:sanctum']], function(){
+    Route::get('user-profile', [AuthController::class, 'userProfile']);
+    Route::apiResource('tasks', TaskController::class)->names('api.tasks');
+    Route::get('auth/logout', [AuthController::class, 'logout']);
 });
+
+Route::get('users', [AuthController::class, 'allUsers']);
